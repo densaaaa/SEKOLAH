fe096ff1125519809b3d59162512c3d77b4fa826
admin01 <admin01@gmail.com>
2025-04-21 12:53:50 +0700

    Update tampilan dan struktur file: - Ubah nama dashboard_guru.php jadi profile_guru.php - Ubah nama dashboard_siswa.php jadi profile_siswa.php - Edit konten dataguru.php

--- dataguru.php
+++ dataguru.php
@@ -25,7 +25,7 @@ $result = mysqli_query($db, $query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manajemen Guru</title>
+    <title>ManajemeN Guru</title>
     <link rel="stylesheet" href="css/dataguru.css?=v10">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
